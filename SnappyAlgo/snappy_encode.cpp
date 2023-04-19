2ecaa5913688e5f96dddc9d6814da2381275bdfa
Shivangi <shivi09@bu.edu>
2023-04-19 01:32:15 -0400

    Updated Snappy

---
+++ SnappyAlgo/snappy_encode.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include <chrono>
+#include <snappy.h>
+
+int main() {
+    // Read input from file
+    std::ifstream input("./Workload/workload.txt", std::ios::binary);
+    std::string data((std::istreambuf_iterator<char>(input)),
+                     std::istreambuf_iterator<char>());
+    input.close();
+
+    // Encode data
+    std::string encoded;
+    auto start = std::chrono::high_resolution_clock::now();
+    snappy::Compress(data.data(), data.size(), &encoded);
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+
+    // Save encoded data to file
+    std::ofstream output("./SnappyAlgo/encoded.txt", std::ios::binary);
+    output.write(encoded.data(), encoded.size());
+    output.close();
+
+    // Print duration
+    std::cout << "Encoding took " << duration.count() << " microseconds." << std::endl;
+
+    return 0;
+}
